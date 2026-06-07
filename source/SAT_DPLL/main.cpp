dc0566f9abb7502d49f73fafd8002daac8a85c59
Vladimir <vova15233@mail.ru>
2026-06-07 23:16:11 +0700

    Добавлен запуск эксперимента по аргументу командной строки

--- source/SAT_DPLL/main.cpp
+++ source/SAT_DPLL/main.cpp
@@ -15,7 +15,10 @@
 
 int main(int argc, char *argv[])
 {
-    AllocatorExperiment::run();
+    if (argc > 1 && std::strcmp(argv[1], "--allocator-experiment") == 0) {
+        AllocatorExperiment::run();
+        return 0;
+    }
 
     QStringList full_file_list;
     QList<QStringList> Elements;
@@ -54,10 +57,6 @@ int main(int argc, char *argv[])
         QString rootvec = "";
         QString rootdnc = "";
 
-        //Строим интервал в которм все компоненты принимают значение '-',
-        //который представляет собой корень уравнения, пока пустой.
-        //В процессе поиска корня, компоненты интервала буду заменены на конкретные значения.
-
         for (int i = 0; i < rangInterval; i++) {
             rootvec += "0";
             rootdnc += "1";
@@ -69,25 +68,9 @@ int main(int argc, char *argv[])
         QByteArray d = rootdnc.toUtf8();
         BBV dnc(d.data());
 
-        // Создаем пустой корень уравнения;
         BoolInterval *root = new BoolInterval(vec, dnc);
 
         BoolEquation *boolequation = new BoolEquation(CNF, root, cnfSize, cnfSize, vec);
-
-        // Алгоритм поиска корня. Работаем всегда с верхушкой стека.
-        // Шаг 1. Правила выполняются? Нет - Ветвление Шаг 5. Да - Упрощаем Шаг 2.
-        // Шаг 2. Строки закончились? Нет - Шаг1, Да - Корень найден? Да - Успех КОНЕЦ, Нет - Шаг 3.
-        // Шаг 3. Кол-во узлов в стеке > 1? Нет - Корня нет КОНЕЦ, Да - Шаг 4.
-        // Шаг 4. Текущий узел выталкиваем из стека, попадаем в новый узел. У нового узла lt rt отличны от NULL? Нет - Шаг 1. Да - Шаг 3.
-        // Шаг 5. Выбор компоненты ветвления, создание двух новых узлов, добавление их в стек сначала с 1 потом с 0. Шаг 1.
-
-        // Алгоритм CheckRules.
-        // Цикл по строкам КНФ.
-        // 1. Проверка правила 2. Выполнилось? Да - Корня нет, Нет - Идем дальше.
-        // 2. Проверка правила 1. Выполнилось? Да - Упрощаем, Нет - Идем дальше.
-
-        // Создаем стек под узлы булева дерева
-        // QStack<NodeBoolTree> BoolTree;
 
         bool rootIsFinded = false;
         stack<NodeBoolTree *> BoolTree;
@@ -98,33 +81,30 @@ int main(int argc, char *argv[])
             NodeBoolTree *currentNode(BoolTree.top());
 
             if (currentNode->lt == nullptr &&
-                    currentNode->rt == nullptr) { // Если вернулись в обработанный узел
+                    currentNode->rt == nullptr) {
                 BoolEquation *currentEquation = currentNode->eq;
                 bool flag = true;
 
-                // Цикл для упрощения по правилам.
                 while (flag) {
-                    int a = currentEquation->CheckRules(); // Проверка выполнения правил
+                    int a = currentEquation->CheckRules();
 
                     switch (a) {
-                        case 0: { // Корня нет.
+                        case 0: {
                             BoolTree.pop();
                             flag = false;
                             break;
                         }
 
-                        case 1: { // Правило выполнилось, корень найден или продолжаем упрощать.
+                        case 1: {
                             if (currentEquation->count == 0 ||
                                     currentEquation->mask.getWeight() ==
-                                    currentEquation->mask.getSize()) { // Если кончились строки или столбцы, корень найден.
+                                    currentEquation->mask.getSize()) {
                                 flag = false;
-                                rootIsFinded =
-                                    true; // Полагаем, что корень найден, выполняем проверку корня
+                                rootIsFinded = true;
 
                                 for (int i = 0; i < cnfSize; i++) {
-
                                     if (!CNF[i]->isEqualComponent(*currentEquation->root)) {
-                                        rootIsFinded = false;//Корень не найден. Продолжаем искать дальше.
+                                        rootIsFinded = false;
                                         BoolTree.pop();
                                         break;
                                     }
@@ -134,9 +114,7 @@ int main(int argc, char *argv[])
                             break;
                         }
 
-                        case 2: { // Правила не выполнились, ветвление.
-                            // Ветвление, создание новых узлов.
-
+                        case 2: {
                             int indexBranching = currentEquation->ChooseColForBranching();
 
                             BoolEquation *Equation0 = new BoolEquation(*currentEquation);
