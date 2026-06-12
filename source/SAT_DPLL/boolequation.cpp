f0c5fe0b62ceeda09aad5520d04aa5553f0efbfa
Vladimir <vova15233@mail.ru>
2026-06-13 01:49:56 +0700

    Подключен выбор переменной через Strategy

--- source/SAT_DPLL/boolequation.cpp
+++ source/SAT_DPLL/boolequation.cpp
@@ -1,262 +1,242 @@
 #include "boolequation.h"
-#include <vector>
-#include <algorithm>
+
+#include "MinUndefinedSelector.h"
+
 #include <ostream>
 #include <string>
 
-BoolEquation::BoolEquation(BoolInterval **cnf, BoolInterval *root, int cnfSize, int count, BBV mask)
+BoolEquation::BoolEquation(
+    BoolInterval **cnf,
+    BoolInterval *root,
+    int cnfSize,
+    int count,
+    BBV mask,
+    IVariableSelector *variableSelector)
+    : variableSelector_(variableSelector)
 {
-	this->cnf = new BoolInterval*[cnfSize];
+    this->cnf = new BoolInterval*[cnfSize];
 
-	for (int i = 0; i < cnfSize; i++) {
-		this->cnf[i] = cnf[i];
-	}
+    for (int i = 0; i < cnfSize; i++) {
+        this->cnf[i] = cnf[i];
+    }
 
-	this->root = root;
-	this->cnfSize = cnfSize;
-	this->count = count;
-	this->mask = mask;
-
+    this->root = root;
+    this->cnfSize = cnfSize;
+    this->count = count;
+    this->mask = mask;
 }
 
 BoolEquation::BoolEquation(BoolEquation &equation)
+    : variableSelector_(equation.variableSelector_)
 {
-	this->cnf = new BoolInterval*[equation.cnfSize];
+    this->cnf = new BoolInterval*[equation.cnfSize];
 
-	for (int i = 0; i < equation.cnfSize; i++) {
-		this->cnf[i] = equation.cnf[i];
-	}
+    for (int i = 0; i < equation.cnfSize; i++) {
+        this->cnf[i] = equation.cnf[i];
+    }
 
-	this->root = new BoolInterval(equation.root->vec, equation.root->dnc);
-	this->cnfSize = equation.cnfSize;
-	this->count = equation.count;
-	this->mask = equation.mask;
+    this->root = new BoolInterval(equation.root->vec, equation.root->dnc);
+    this->cnfSize = equation.cnfSize;
+    this->count = equation.count;
+    this->mask = equation.mask;
 }
 
 // Проверка правил
 int BoolEquation::CheckRules()
 {
-	BBV rez, rez1, rez0;
-	bool rezInit = false;
+    BBV rez, rez1, rez0;
+    bool rezInit = false;
 
-	for (int i = 0; i < cnfSize; i++) {
-		BoolInterval *interval = cnf[i];
+    for (int i = 0; i < cnfSize; i++) {
+        BoolInterval *interval = cnf[i];
 
-		//		std::cout << string(*interval) << " <---> " << i << endl;
+        //		std::cout << string(*interval) << " <---> " << i << endl;
 
-		if (interval != nullptr) {
-			if (Rule2RowNull(interval)) {
-				return 0;
-			}
+        if (interval != nullptr) {
+            if (Rule2RowNull(interval)) {
+                return 0;
+            }
 
-			if (count == 1) {
-				if (Rule4Col0(interval->vec ^ interval->dnc)) {
-					return 1;
-				}
+            if (count == 1) {
+                if (Rule4Col0(interval->vec ^ interval->dnc)) {
+                    return 1;
+                }
 
-				if (Rule5Col1(interval->vec)) {
-					return 1;
-				}
-			}
+                if (Rule5Col1(interval->vec)) {
+                    return 1;
+                }
+            }
 
-			if (Rule1Row1(interval)) {
-				for (int k = 0; k < interval->length(); k++) {
-					if (mask[k] != 1) {
-						char value = interval->getValue(k);
+            if (Rule1Row1(interval)) {
+                for (int k = 0; k < interval->length(); k++) {
+                    if (mask[k] != 1) {
+                        char value = interval->getValue(k);
 
-						if (value != '-') {
-							if (value == '0') {
-								//Simplify(k, '1');
-								Simplify(k, '0');
-								break;
-							} else {
-								//Simplify(k, '0');
-								Simplify(k, '1');
-								break;
-							}
-						}
-					}
-				}
+                        if (value != '-') {
+                            if (value == '0') {
+                                //Simplify(k, '1');
+                                Simplify(k, '0');
+                                break;
+                            } else {
+                                //Simplify(k, '0');
+                                Simplify(k, '1');
+                                break;
+                            }
+                        }
+                    }
+                }
 
-				return 1;
-			}
+                return 1;
+            }
 
-			if (!rezInit) {
-				// cout << interval->vec;
-				rez0 = interval->vec ^ interval->dnc;
-				rez1 = interval->vec;
-				rez  = interval->dnc;
-				rezInit = true;
-			} else {
-				rez = rez & interval->dnc;
+            if (!rezInit) {
+                // cout << interval->vec;
+                rez0 = interval->vec ^ interval->dnc;
+                rez1 = interval->vec;
+                rez  = interval->dnc;
+                rezInit = true;
+            } else {
+                rez = rez & interval->dnc;
 
-				//cout << rez0;
-				BBV temprez = interval->vec ^ interval->dnc;
-				// cout << temprez;
-				rez0 = rez0 | temprez;
-				// cout << rez0;
+                //cout << rez0;
+                BBV temprez = interval->vec ^ interval->dnc;
+                // cout << temprez;
+                rez0 = rez0 | temprez;
+                // cout << rez0;
 
-				// cout << rez1;
-				rez1 = rez1 & interval->vec;
-				// cout << rez1;
-			}
-		}
-	}
+                // cout << rez1;
+                rez1 = rez1 & interval->vec;
+                // cout << rez1;
+            }
+        }
+    }
 
-	//       cout << "Vector dlya ---- " << rez << "\n";
-	//       cout << "Vector dlya 1 " << rez1 << "\n";
-	//       cout << "Vector dlya 0 " << rez0 << "\n";
-	Rule3ColNull(rez);
+    //       cout << "Vector dlya ---- " << rez << "\n";
+    //       cout << "Vector dlya 1 " << rez1 << "\n";
+    //       cout << "Vector dlya 0 " << rez0 << "\n";
+    Rule3ColNull(rez);
 
-	if (Rule4Col0(rez0)) {
-		return 1;
-	}
+    if (Rule4Col0(rez0)) {
+        return 1;
+    }
 
-	if (Rule5Col1(rez1)) {
-		return 1;
-	}
+    if (Rule5Col1(rez1)) {
+        return 1;
+    }
 
-
-	return 2;
+    return 2;
 }
 
 // Строка пустая
 bool BoolEquation::Rule2RowNull(BoolInterval *interval)
 {
-	int counter = 0;
+    int counter = 0;
 
-	for (int i = 0; i < mask.getSize(); i++) {
-		if (mask[i] != 1) {
-			if (interval->getValue(i) != '-') {
-				counter++;
-				break;
-			}
-		}
-	}
+    for (int i = 0; i < mask.getSize(); i++) {
+        if (mask[i] != 1) {
+            if (interval->getValue(i) != '-') {
+                counter++;
+                break;
+            }
+        }
+    }
 
-	if (counter > 0) {
-		return false;
-	}
+    if (counter > 0) {
+        return false;
+    }
 
-	return true;
+    return true;
 }
+
 //Строка содержит одну переменную
 bool BoolEquation::Rule1Row1(BoolInterval *interval)
 {
-	int counter = 0;
+    int counter = 0;
 
-	for (int i = 0; i < mask.getSize(); i++) {
-		if (mask[i] != 1) {
-			if (interval->getValue(i) != '-') {
-				counter++;
-			}
-		}
-	}
+    for (int i = 0; i < mask.getSize(); i++) {
+        if (mask[i] != 1) {
+            if (interval->getValue(i) != '-') {
+                counter++;
+            }
+        }
+    }
 
-	if (counter == 1) {
-		return true;
-	}
+    if (counter == 1) {
+        return true;
+    }
 
-	return false;
+    return false;
 }
 
 void BoolEquation::Rule3ColNull(BBV vector)
 {
-
-	//cout << "Vector "<< vector;
-	//cout << "Mask " << mask;
-	for (int i = 0; i < vector.getSize(); i++) {
-		if (vector[i] == 1 && mask[i] != 1) {
-			mask.Set1(i);
-		}
-	}
-
+    //cout << "Vector "<< vector;
+    //cout << "Mask " << mask;
+    for (int i = 0; i < vector.getSize(); i++) {
+        if (vector[i] == 1 && mask[i] != 1) {
+            mask.Set1(i);
+        }
+    }
 }
 
 bool BoolEquation::Rule4Col0(BBV vector)
 {
-	for (int i = 0; i < vector.getSize(); i++) {
-		if (vector[i] == 0 && mask[i] != 1) {
-			//			Simplify(i, '1');
-			Simplify(i, '0');
-			return true;
-		}
-	}
+    for (int i = 0; i < vector.getSize(); i++) {
+        if (vector[i] == 0 && mask[i] != 1) {
+            //			Simplify(i, '1');
+            Simplify(i, '0');
+            return true;
+        }
+    }
 
-	return false;
+    return false;
 }
 
 bool BoolEquation::Rule5Col1(BBV vector)
 {
-	for (int i = 0; i < vector.getSize(); i++) {
-		if (vector[i] == 1 && mask[i] != 1) {
-			//			Simplify(i, '0');
-			Simplify(i, '1');
-			return true;
-		}
-	}
+    for (int i = 0; i < vector.getSize(); i++) {
+        if (vector[i] == 1 && mask[i] != 1) {
+            //			Simplify(i, '0');
+            Simplify(i, '1');
+            return true;
+        }
+    }
 
-	return false;
+    return false;
 }
 
 void BoolEquation::Simplify(int ixCol, char value)
 {
-	for (int i = 0; i < cnfSize; i++) {
-		BoolInterval *interval = cnf[i];
+    for (int i = 0; i < cnfSize; i++) {
+        BoolInterval *interval = cnf[i];
 
-		if (interval != nullptr) {
-			char val = interval->getValue(ixCol);
+        if (interval != nullptr) {
+            char val = interval->getValue(ixCol);
 
-			//if (val != value && val != '-') {
-			if (val == value && val != '-') {
-				cnf[i] = nullptr;
-				count--;
-			}
-		}
-	}
+            //if (val != value && val != '-') {
+            if (val == value && val != '-') {
+                cnf[i] = nullptr;
+                count--;
+            }
+        }
+    }
 
-	root->setValue(value, ixCol);
-	mask.Set1(ixCol);
+    root->setValue(value, ixCol);
+    mask.Set1(ixCol);
+}
+
+void BoolEquation::setVariableSelector(IVariableSelector *variableSelector)
+{
+    variableSelector_ = variableSelector;
 }
 
 int BoolEquation::ChooseColForBranching()
 {
-	vector<int> indexes;
-	vector<int> values;
-	bool rezInit = false;
+    static MinUndefinedSelector defaultSelector;
 
-	for (int i = 0; i < mask.getSize(); i++) {
-		if (mask[i] == 0) {
-			indexes.push_back(i);
-		}
-	}
+    if (variableSelector_ == nullptr) {
+        return defaultSelector.selectVariable(*this);
+    }
 
-	for (int i = 0; i < cnfSize; i++) {
-		BoolInterval *interval = cnf[i];
-
-		if (interval != nullptr) {
-			if (!rezInit) {
-				for (int k = 0; k < indexes.size(); k++) {
-					if (interval->getValue(indexes.at(k)) == '-') {
-						values.push_back(1);
-					} else {
-						values.push_back(0);
-					}
-				}
-
-				rezInit = true;
-			} else {
-				for (int k = 0; k < indexes.size(); k++) {
-					if (interval->getValue(indexes.at(k)) == '-') {
-						//int val = values.at(k) + (interval->getValue(indexes.at(k)) - '0');
-						values.at(k)++;
-					}
-				}
-			}
-		}
-	}
-
-	int minElementIndex = std::min_element(values.begin(), values.end()) - values.begin();
-
-	return indexes.at(minElementIndex);
+    return variableSelector_->selectVariable(*this);
 }
--- source/SAT_DPLL/boolequation.h
+++ source/SAT_DPLL/boolequation.h
@@ -2,25 +2,41 @@
 #define BOOLEQUATION_H
 
 #include "boolinterval.h"
+#include "IVariableSelector.h"
 
 class BoolEquation
 {
 public:
-	BoolInterval **cnf;//множество интервалов
-	BoolInterval *root;//Корень уравнения
-	int cnfSize; // Размер КНФ
-	int count; //количество дизъюнкций
-	BBV mask; //маска для столбцов
-	BoolEquation(BoolInterval **cnf, BoolInterval *root, int cnfSize, int count, BBV mask);
-	BoolEquation(BoolEquation &equation);
-	int CheckRules();
-	bool Rule1Row1(BoolInterval *interval);
-	bool Rule2RowNull(BoolInterval *interval);
-	void Rule3ColNull(BBV vector);
-	bool Rule4Col0(BBV vector);
-	bool Rule5Col1(BBV vector);
-	void Simplify(int ixCol, char value);
-	int ChooseColForBranching();
+    BoolInterval **cnf;
+    BoolInterval *root;
+    int cnfSize;
+    int count;
+    BBV mask;
+
+    BoolEquation(
+        BoolInterval **cnf,
+        BoolInterval *root,
+        int cnfSize,
+        int count,
+        BBV mask,
+        IVariableSelector *variableSelector = nullptr
+    );
+
+    BoolEquation(BoolEquation &equation);
+
+    int CheckRules();
+    bool Rule1Row1(BoolInterval *interval);
+    bool Rule2RowNull(BoolInterval *interval);
+    void Rule3ColNull(BBV vector);
+    bool Rule4Col0(BBV vector);
+    bool Rule5Col1(BBV vector);
+    void Simplify(int ixCol, char value);
+
+    void setVariableSelector(IVariableSelector *variableSelector);
+    int ChooseColForBranching();
+
+private:
+    IVariableSelector *variableSelector_;
 };
 
 #endif // BOOLEQUATION_H
